1b965ea37a92fd7d08dfa7daf58f5645dd3c93fc
PosztoHUN <97360183+PosztoHUN@users.noreply.github.com>
2024-03-06 17:28:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function (){
+    setInterval (function(){
+        var date = new Date()
+        var displayDate = date.toLocaleDateString();
+        var displayTime = date.toLocaleTimeString();
+        document.getElementById('datetime').innerHTML = displayDate + " " + displayTime;
+    }, 1000); // 1000 milliseconds = 1 second
+}
